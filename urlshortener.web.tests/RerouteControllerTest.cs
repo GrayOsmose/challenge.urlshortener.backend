214812fae3ed9051367abd09d6bed5265cae1f29
Sergey Zalubovskiy <osmosegw@gmail.com>
2017-03-18 20:54:05 +0300

    unit tests drafts for controllers

---
+++ urlshortener.web.tests/RerouteControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using urlshortener.web.Controllers;
+using Xunit;
+
+namespace urlshortener.web.tests
+{
+    public class RerouteControllerTest
+    {
+        private readonly RerouteController _controller;
+
+        public RerouteControllerTest()
+        {
+            // ToDo : find moq for core 1.1
+            // _controller = new RerouteController()
+        }
+
+        [Fact]
+        public void InGetWithKeyValidationPass()
+        {
+            // Check key validation
+            // method should be called
+        }
+
+        [Fact]
+        public void InGetWithKeyValidationFail()
+        {
+            // Check key validation, should be invalid
+            // method shouldn't be called
+        }
+
+        [Fact]
+        public void InGetModelFromManagerPass()
+        {
+            // Check url model returned
+            // method should be called
+        }
+
+        [Fact]
+        public void InGetModelFromManagerFailModel()
+        {
+            // Check url model returned, should be null and throw exceptiopn
+            // method shouldn't be called
+        }
+    }
+}
---
+++ urlshortener.web.tests/UrlControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using urlshortener.web.Controllers;
+using Xunit;
+
+namespace urlshortener.web.tests
+{
+    public class UrlControllerTest
+    {
+        private readonly UrlController _controller;
+
+        public UrlControllerTest()
+        {
+            // ToDo : find moq for core 1.1
+            // _controller = new UrlController()
+        }
+
+        [Fact]
+        public void InPostWithModelValidationPass()
+        {
+            // Check url model validation
+            // method should be called
+        }
+
+        [Fact]
+        public void InPostWithModelValidationFailUrl()
+        {
+            // Check url model validation, should be invalid
+            // method shouldn't be called
+        }
+
+        [Fact]
+        public void InDeleteWithKeyValidationPass()
+        {
+            // Check key validation
+            // method should be called
+        }
+
+        [Fact]
+        public void InDeleteWithValidationFail()
+        {
+            // Check key validation
+            // method shouldn't be called
+        }
+    }
+}
